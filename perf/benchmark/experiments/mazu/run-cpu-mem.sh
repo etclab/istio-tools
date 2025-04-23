fbf40862211ccb87cc1fade5d5ea486087bc745c
Aashutosh Poudel <aashutoshpoudyal@gmail.com>
2025-04-23 16:33:46 -0400

    add initial configs for mazu setup will be different

---
+++ perf/benchmark/experiments/mazu/run-cpu-mem.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Save the current directory and move up two levels
+pushd ../../ > /dev/null || { echo "Failed to change directory"; exit 1; }
+
+# Loop over durations
+for duration in 120 240 480; do
+    config_path="./experiments/mazu/cpu_mem_${duration}s.yaml"
+    
+    if [[ -f $config_path ]]; then
+        echo "▶ Running benchmark for duration=${duration}s"
+        python runner/runner.py --config_file "$config_path"
+    else
+        echo "⚠️ Config file not found: $config_path"
+    fi
+done
+
+# Return to original directory
+popd > /dev/null || exit
---
+++ perf/benchmark/experiments/mazu/script.sh
@@ -0,0 +1,8 @@
+# run the benchmarks
+python runner/runner.py --config_file ./experiments/mazu/cpu_mem.yaml
+
+# get the results
+python ./runner/fortio.py $FORTIO_CLIENT_URL --prometheus=$PROMETHEUS_URL --csv StartTime,ActualDuration,Labels,NumThreads,ActualQPS,p50,p90,p99,p999,cpu_mili_avg_istio_proxy_fortioclient,cpu_mili_avg_istio_proxy_fortioserver,cpu_mili_avg_istio_proxy_istio-ingressgateway,mem_Mi_avg_istio_proxy_fortioclient,mem_Mi_avg_istio_proxy_fortioserver,mem_Mi_avg_istio_proxy_istio-ingressgateway
+
+
+# plot the results
