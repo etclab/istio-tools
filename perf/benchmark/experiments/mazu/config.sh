70878986edd0b8cd80d93ba7058ed927c628cc91
Aashutosh Poudel <apoudel@pc845.emulab.net>
2025-05-09 09:10:00 -0600

    add new data

--- perf/benchmark/experiments/mazu/config.sh
+++ perf/benchmark/experiments/mazu/config.sh
@@ -4,4 +4,6 @@ DURATIONS=(240)
 # proxy workers = 2 or unset
 # EXTRA_LABELS=("w2" "w_unset")
 # EXTRA_LABELS=("nc_false") # nocatchup=false
-EXTRA_LABELS=("unc_false") # uniform nocatchup both false
+# EXTRA_LABELS=("unc_false") # uniform nocatchup both false
+EXTRA_LABELS=("ka_true_unc_true") # uniform nocatchup both false
+
